5cfa2f42840e31cd550bb561946dc625905fa4d6
Dominique Luna <dluna132@gmail.com>
2020-01-11 15:17:32 -0500

    update

--- startup.jl
+++ startup.jl
@@ -1,9 +1,14 @@
 push!(LOAD_PATH, "$(homedir())/.julia/dev")
-atreplinit() do repl
-    try
-        @eval using Revise
-        @async Revise.wait_steal_repl_backend()
-    catch
-    end
-end
 
+# atreplinit() do repl
+#     try
+#         @eval using Pkg
+#         haskey(Pkg.installed(), "Revise") || @eval Pkg.add("Revise")
+#     catch
+#     end
+#     try
+#         @eval using Revise
+#         @async Revise.wait_steal_repl_backend()
+#     catch
+#     end
+# end
