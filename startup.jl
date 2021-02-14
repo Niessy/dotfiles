23c4a51c8c1e66d0e215dd631c89d8e9d98d7682
Dominique Luna <dluna132@gmail.com>
2021-02-13 19:18:59 -0500

    stuff

--- startup.jl
+++ startup.jl
@@ -1,6 +1,19 @@
 push!(LOAD_PATH, "$(homedir())/.julia/dev")
-push!(LOAD_PATH, "$(homedir())/work")
+push!(LOAD_PATH, "$(homedir())/work/ocean/mirage")
 
 using OhMyREPL
 colorscheme!("GruvboxDark")
 using Revise
+
+# import REPL
+# import REPL.LineEdit
+#
+# const mykeys = Dict{Any,Any}(
+#     "F10" => "~",
+# )
+#
+# function customize_keys(repl)
+#     repl.interface = REPL.setup_interface(repl; extra_repl_keymap = mykeys)
+# end
+#
+# atreplinit(customize_keys)
