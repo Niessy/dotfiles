12c2dc01e143eea7ec45e22c467b6b580a1764ff
Dominique Luna <dluna132@gmail.com>
2021-02-13 19:19:10 -0500

    ok

--- startup.jl
+++ startup.jl
@@ -4,16 +4,3 @@ push!(LOAD_PATH, "$(homedir())/work/ocean/mirage")
 using OhMyREPL
 colorscheme!("GruvboxDark")
 using Revise
-
-# import REPL
-# import REPL.LineEdit
-#
-# const mykeys = Dict{Any,Any}(
-#     "F10" => "~",
-# )
-#
-# function customize_keys(repl)
-#     repl.interface = REPL.setup_interface(repl; extra_repl_keymap = mykeys)
-# end
-#
-# atreplinit(customize_keys)
